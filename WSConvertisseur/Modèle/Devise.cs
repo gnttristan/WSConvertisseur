5d9b6a450ecb0fa4461d55951aea8955eaf8c50f
XXGIGASHADOWPAUL38XX <108007727+XXGIGASHADOWPAUL38XX@users.noreply.github.com>
2023-01-25 14:49:54 +0100

    azerazer

---
+++ WSConvertisseur/Modèle/Devise.cs
@@ -0,0 +1,41 @@
+﻿namespace WSConvertisseur.Modèle
+{
+    public class Devise
+    {
+        private int id;
+        private string nomDevise;
+        private double taux;
+
+        public Devise()
+        {
+
+        }
+
+        public Devise(int id, string nomDevise, double taux)
+        {
+            this.id = Id;
+            this.nomDevise = NomDevise;
+            this.taux = Taux;
+        }
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string NomDevise
+        {
+            get { return nomDevise; }
+            set { nomDevise = value; }
+        }
+
+        public double Taux
+        {
+            get { return taux; }
+            set { taux = value; }
+        }
+
+
+    }
+}
